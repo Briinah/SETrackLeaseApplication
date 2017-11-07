d27469c90b6145d7beb2c9ed56484ca55a665803
setrackjava <maarten.stienstra@gmail.com>
2017-11-07 15:47:52 +0100

    vehicle class
    Added variables + constructor

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,47 @@
+
+/*
+This class represents a vehicle
+Authors: Carolien en Maarten
+ */
+
 public class Vehicle {
+
+    private String brand;
+    private String type;
+    private String licensePlate;
+
+    private int price;
+    private int manufactureDate;
+    private int tax;
+
+    private float mileageInKm;
+
+    // represents different types of fuel
+    public enum FuelType {
+        Gasoline, Diesel, Electric
+    }
+
+    // represents different types of vehicles
+    public enum VehicleType {
+        Car, Motor, Truck
+    }
+
+    public FuelType fuelType;
+    public VehicleType vehicleType;
+
+    // initialises new vehicle
+    public Vehicle(String newBrand, String newType, String newLicensePlate, int newManufactureDate, int newTax, FuelType        newFuelType, VehicleType newVehicleType) {
+        this.brand = newBrand;
+        this.type = newType;
+        this.licensePlate = newLicensePlate;
+        this.manufactureDate = newManufactureDate;
+        this.tax = newTax;
+        this.mileageInKm = 0;
+        this.fuelType = newFuelType;
+        this.vehicleType = newVehicleType;
+
+    }
+
+
+
 }
