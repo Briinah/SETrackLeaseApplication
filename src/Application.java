fcfab68897ce61d271efc754f47cfc1ee0d11e99
CORP\jscheide <jeffreyscheidelaar@gmail.com>
2017-11-10 15:32:41 +0100

    Added line to Application

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,6 @@
 
 import company.*;
+import leasecompany.Contract;
 import leasecompany.LeaseCompany;
 import leasecompany.vehicles.Car;
 import leasecompany.vehicles.FuelType;
@@ -19,21 +20,28 @@ public class Application {
     public static void setDatabase()
     {
         Company company = new Company();
-        LeaseCompany ls = new LeaseCompany(5,5);
+        LeaseCompany ls = new LeaseCompany(10,10);
 
         Employee jeffrey = new Employee("Jeffrey","Scheidelaar",1,company);
         Employee Bas = new Employee("Bas","Weeterings",1,company);
         company.addEmployee(jeffrey);
         company.addEmployee(Bas);
 
-        Vehicle BMW = new Car("BMW","I3","GHFD45",LocalDate.of(2015,2,10),22, FuelType.Gasoline);
-        Vehicle Audi = new Car("Audi","A1","HH88FF",LocalDate.of(2016,8,14),22, FuelType.Diesel);
+        Employee daniel = company.getEmployeeList()[0];
+        
+        Vehicle BMWBas = new Car("BMW","I3","GHFD45",LocalDate.of(2015,2,10),22, FuelType.Gasoline);
+        Vehicle AudiJeff = new Car("Audi","A1","HH88FF",LocalDate.of(2016,8,14),22, FuelType.Diesel);
+        Vehicle AudiDan = new Car("Audi","A3","FF99HH",LocalDate.of(2016,4,28),22, FuelType.Diesel);
         ls.addVehicle(BMW);
         ls.addVehicle(Audi);
 
+        Contract conJeff = new Contract(LocalDate.now(),LocalDate.of(2021,20,7),jeffrey,AudiJeff);
+        Contract conBas = new Contract(LocalDate.now(),LocalDate.of(2021,20,11),Bas,BMWBas);
+        Contract conDan = new Contract(LocalDate.now(),LocalDate.of(2021,26,12),Bas,AudiDan);
 
-
-
+        ls.addContract(conJeff);
+        ls.addContract(conBas);
+        ls.addContract(conDan);
 
     }
 }
