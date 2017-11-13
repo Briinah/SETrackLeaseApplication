2dce2c658cc14568c48a5a9384af195f6a138d50
Carolien Gilbers <carolien.gilbers@capgemini.com>
2017-11-13 16:27:58 +0100

    Added 3 buttons to the main panel and added a sout to each button event

--- src/ui/panels/MainPanel.java
+++ src/ui/panels/MainPanel.java
@@ -1,14 +1,59 @@
 package ui.panels;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPanel extends Panel {
 
     Button companyButton;
+    Button employeeButton;
+    Button leaseCompanyButton;
 
+    /**
+     * Main panel with buttons to company, employee and lease company
+     */
     public MainPanel(){
 
+        // set layout
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10); // margins between buttons
+
+        // initialize buttons
         companyButton = new Button("Companies");
-        this.add(companyButton);
+        employeeButton = new Button("Employees");
+        leaseCompanyButton = new Button("Lease Companies");
+
+        // add buttons to panel
+        this.add(companyButton, gbc);
+        this.add(employeeButton, gbc);
+        this.add(leaseCompanyButton, gbc);
+
+        // set company button event
+        companyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked company button!");
+            }
+        });
+
+        // set employee button event
+        employeeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked employee button!");
+            }
+        });
+
+        // set lease company button event
+        leaseCompanyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked lease company button!");
+            }
+        });
     }
 }
