3d946f17bb0e6f9c02d9dc5689055ea8b768d403
CORP\jscheide <jeffreyscheidelaar@gmail.com>
2017-11-14 16:04:41 +0100

    Namen aangepast naar type

--- src/ui/panels/CompanyItemPanel.java
+++ src/ui/panels/CompanyItemPanel.java
@@ -15,27 +15,24 @@ import leasecompany.*;
 public class CompanyItemPanel extends Panel{
 
 
-    private Button addEmployee,delEmployee,addLeaseCompany,delLeaseCompany;
-    private Label employeeLabel, leaseCompanyLabel;
+    private Button addEmployee_B,delEmployee_B,addLeaseCompany_B,delLeaseCompany_B;
+    private Label employee_Label, leaseCompany_Label;
     private List employeeList,leaseCompanieList;
     private GridBagLayout layout;
     Company companyClass = new Company();
-    final TextField fullNameText,leaseCompanyText;
-
-
-
+    final TextField fullName_Text,leaseCompany_Text;
 
 
     public CompanyItemPanel() {
 
-        employeeLabel = new Label("Employees ");
-        leaseCompanyLabel = new Label("Lease company's ");
-        addEmployee = new Button("Add employee");
-        delEmployee = new Button("Delete employee");
-        addLeaseCompany = new Button("Add company");
-        delLeaseCompany = new Button("Delete company");
-        fullNameText = new TextField(25);
-        leaseCompanyText = new TextField(25);
+        employee_Label = new Label("Employees ");
+        leaseCompany_Label = new Label("Lease company's ");
+        addEmployee_B = new Button("Add employee");
+        delEmployee_B = new Button("Delete employee");
+        addLeaseCompany_B = new Button("Add company");
+        delLeaseCompany_B = new Button("Delete company");
+        fullName_Text = new TextField(25);
+        leaseCompany_Text = new TextField(25);
 
         employeeList = new List();
         leaseCompanieList = new List();
@@ -44,21 +41,21 @@ public class CompanyItemPanel extends Panel{
         this.setLayout(layout);
         GridBagConstraints gbc = new GridBagConstraints();
 
-        employeeLabel.setVisible(true);
-        leaseCompanyLabel.setVisible(true);
-        addEmployee.setVisible(true);
-        delEmployee.setVisible(true);
+        employee_Label.setVisible(true);
+        leaseCompany_Label.setVisible(true);
+        addEmployee_B.setVisible(true);
+        delEmployee_B.setVisible(true);
         employeeList.setVisible(true);
         leaseCompanieList.setVisible(true);
-        fullNameText.setVisible(true);
+        fullName_Text.setVisible(true);
 
         gbc.fill = GridBagConstraints.CENTER;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        this.add(employeeLabel,gbc);
+        this.add(employee_Label,gbc);
         gbc.gridx = 4;
         gbc.gridy = 0;
-        this.add(leaseCompanyLabel,gbc);
+        this.add(leaseCompany_Label,gbc);
 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.ipady = 150;
@@ -73,33 +70,33 @@ public class CompanyItemPanel extends Panel{
         gbc.ipady = 10;
         gbc.gridx = 0;
         gbc.gridy = 2;
-        this.add(fullNameText,gbc);
+        this.add(fullName_Text,gbc);
         gbc.gridx = 4;
         gbc.gridy = 2;
-        this.add(leaseCompanyText,gbc);
+        this.add(leaseCompany_Text,gbc);
 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10,0,0,110);
         gbc.ipady = 10;
         gbc.gridx = 0;
         gbc.gridy = 3;
-        this.add(delEmployee,gbc);
+        this.add(delEmployee_B,gbc);
 
         gbc.insets = new Insets(10,110,0,0);
         gbc.gridx = 0;
         gbc.gridy = 3;
-        this.add(addEmployee,gbc);
+        this.add(addEmployee_B,gbc);
 
         gbc.insets = new Insets(10,0,0,105);
         gbc.ipady = 10;
         gbc.gridx = 4;
         gbc.gridy = 3;
 
-        this.add(delLeaseCompany,gbc);
+        this.add(delLeaseCompany_B,gbc);
         gbc.insets = new Insets(10,105,0,0);
         gbc.gridx = 4;
         gbc.gridy = 3;
-        this.add(addLeaseCompany,gbc);
+        this.add(addLeaseCompany_B,gbc);
 
         Handlers();
     }
@@ -107,10 +104,10 @@ public class CompanyItemPanel extends Panel{
     // Buttons for adding and deleting Items
     private void Handlers(){
 
-        addEmployee.addActionListener(new ActionListener() {
+        addEmployee_B.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String name = fullNameText.getText();
+                String name = fullName_Text.getText();
                 if(!name.contains(" "))
                     return;
                 String[] fullName = name.split(" ");
@@ -119,23 +116,23 @@ public class CompanyItemPanel extends Panel{
             }
         });
 
-        delEmployee.addActionListener(new ActionListener() {
+        delEmployee_B.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 delEmployeeFromList(employeeList.getSelectedIndex());
             }
         });
 
-        addLeaseCompany.addActionListener(new ActionListener() {
+        addLeaseCompany_B.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String company = leaseCompanyText.getText();
+                String company = leaseCompany_Text.getText();
                 LeaseCompany ls = new LeaseCompany(company,5,5);
                 addLeaseCompanyToList(company,ls);
             }
         });
 
-        delLeaseCompany.addActionListener(new ActionListener() {
+        delLeaseCompany_B.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 delLeaseCompanyFromList(leaseCompanieList.getSelectedIndex());
